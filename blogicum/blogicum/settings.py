ae20e565d413f0bb7145552eb52da4004eb7d617
Ксения Демиха <arestovaKse@yandex.ru>
2025-06-10 00:08:51 +0300

    Первое

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -125,8 +125,8 @@ CSRF_FAILURE_VIEW = 'pages.views.csrf_failure'
 
 LOGIN_REDIRECT_URL = 'blog:index'
 
-MEDIA_ROOT = BASE_DIR / 'media' 
+MEDIA_ROOT = BASE_DIR / 'media'
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 
-EMAIL_FILE_PATH = BASE_DIR / 'sent_emails' 
+EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
